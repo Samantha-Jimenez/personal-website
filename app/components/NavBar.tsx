ed9082684873cc7387f0d2798b6afbc0a5500b9b
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2024-12-30 02:22:28 -0500

    fix nav tooltip z-index

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -48,28 +48,28 @@ const NavBar = () => {
       </div>
       <div className="flex-none self-start">
         <ul className="menu menu-horizontal px-1 justify-items-center max-[662px]:grid max-[662px]:grid-flow-row max-[662px]:grid-cols-6 max-[662px]:grid-rows-2">
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="instagram">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="instagram">
             <li>
               <a className="hover:scale-150 hover:bg-transparent active:bg-white active:dark:bg-neutral-200 transition-transform duration-200" href="https://www.instagram.com/aka.jimena" target="_blank" rel="noopener noreferrer">
                 <ThinInstagramIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="threads">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="threads">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.threads.net/@aka.jimena" target="_blank" rel="noopener noreferrer">
                 <ThinThreadsIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="tiktok">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="tiktok">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.tiktok.com/@aka.jimena" target="_blank" rel="noopener noreferrer">
                 <ThinTikTokIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="youtube">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="youtube">
             <li>
               <div 
                   className="hover:scale-150 hover:bg-transparent transition-transform duration-200 cursor-pointer" 
@@ -79,35 +79,35 @@ const NavBar = () => {
               </div>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="goodreads">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="goodreads">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.goodreads.com/user/show/183397302-samantha" target="_blank" rel="noopener noreferrer">
                 <GoodReadsIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="spotify">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="spotify">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://open.spotify.com/user/jimenamorenita?si=2fa3b2d8e7d147d1" target="_blank" rel="noopener noreferrer">
                 <ThinSpotifyIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="email">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="email">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="mailto:SamanthaB.Jimenez@gmail.com" target="_blank" rel="noopener noreferrer">
                 <ThinGoogleMailIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="linkedin">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="linkedin">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.linkedin.com/in/samanthabjimenez/" target="_blank" rel="noopener noreferrer">
                 <ThinLinkedInIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="github">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="github">
             <li>
               <div 
                 className={`active:bg-white active:dark:bg-neutral-200 hover:scale-150 hover:bg-transparent transition-transform duration-200 cursor-pointer w-[104%] hover:z-[12] ${openGithubMenu ? 'scale-150 open-menu' : ''}`} 
@@ -117,14 +117,14 @@ const NavBar = () => {
                 <span className={`ml-1 arrow-icon icon-[arcticons--emoji-arrow-pointing-rightwards-then-curving-downwards] left-[60%]`}></span>
               </div>
               {openGithubMenu && (
-                <ul className={`ml-[-80%] absolute dark:bg-zinc-900 bg-neutral-200 p-2 text-xs w-max rounded-lg top-[35px] z-[11] pt-5`}>
+                <ul className={`ml-[-80%] absolute dark:bg-zinc-900 bg-neutral-200 p-2 text-xs w-max rounded-lg top-[35px] z-[12] pt-5`}>
                     <li className="hover:bg-gray-300 hover:dark:bg-[#2A323C] rounded"><a href="https://github.com/samantha-jimenez" target="_blank" rel="noopener noreferrer">Current Account</a></li>
                     <li className="hover:bg-gray-300 hover:dark:bg-[#2A323C] rounded"><a href="https://github.com/samanthabjimenez" target="_blank" rel="noopener noreferrer">Previous Account</a></li>
                 </ul>
               )}
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="portfolio">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="portfolio">
             <li>
               <div 
                 className={`hover:scale-150 hover:bg-transparent transition-transform duration-200 cursor-pointer w-[104%] hover:z-[12] ${openPortfolioMenu ? 'scale-150 open-menu' : ''}`} 
@@ -134,21 +134,21 @@ const NavBar = () => {
                 <span className={`ml-1 arrow-icon icon-[arcticons--emoji-arrow-pointing-rightwards-then-curving-downwards] left-[60%]`}></span>
               </div>
               {openPortfolioMenu && (
-                <ul className={`ml-[-80%] absolute dark:bg-zinc-900 bg-neutral-200 p-2 text-xs w-max rounded-lg top-[35px] z-[11] pt-5`}>
+                <ul className={`ml-[-80%] absolute dark:bg-zinc-900 bg-neutral-200 p-2 text-xs w-max rounded-lg top-[35px] z-[12] pt-5`}>
                   <li className="hover:bg-gray-300 hover:dark:bg-[#2A323C] rounded"><a href="https://samantha-jimenez.com/" target="_blank" rel="noopener noreferrer">Current Portfolio</a></li>
                   <li className="hover:bg-gray-300 hover:dark:bg-[#2A323C] rounded"><a href="https://samantha-jimenez.netlify.app/" target="_blank" rel="noopener noreferrer">Previous Portfolio</a></li>
                 </ul>
               )}
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="mvmnt collectives">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="mvmnt collectives">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.mvmntcollectives.com/" target="_blank" rel="noopener noreferrer">
                 <ThinMvmntCollectivesIcon />
               </a>
             </li>
           </div>
-          <div className="tooltip tooltip-bottom z-[12]" data-tip="strava">
+          <div className="tooltip tooltip-bottom hover:z-[12]" data-tip="strava">
             <li>
               <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://strava.app.link/lxN0E2NT0Ob" target="_blank" rel="noopener noreferrer">
                 <ThinStravaIcon />
