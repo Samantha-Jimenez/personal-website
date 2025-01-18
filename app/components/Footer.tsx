0c18ad7bc98a8f8b5d114e3bc8a3bc0490b3c3b9
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-01-17 22:32:00 -0500

    rearrange icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
             <p className="montserrat-mine font-light">Copyright © {new Date().getFullYear()} - All right reserved</p>
         </aside> */}
         <nav className="flex-none w-full justify-end grid-flow-col gap-6 grid-rows-1 md:justify-self-end max-[500px]:justify-center">
-            <ul className="menu menu-horizontal px-1 grid-flow-row grid grid-rows-1 grid-cols-12 max-[662px]:grid-cols-6 max-[662px]:grid-rows-2 max-[662px]:justify-self-end">
+            <ul className="menu menu-horizontal px-1 grid-flow-row grid grid-rows-1 grid-cols-12 max-[662px]:grid-cols-5 max-[662px]:grid-rows-2 max-[662px]:justify-self-end">
                 <div className="tooltip tooltip-top justify-items-center" data-tip="instagram">
                     <li>
                     <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.instagram.com/aka.jimena" target="_blank" rel="noopener noreferrer">
@@ -62,13 +62,13 @@ const Footer = () => {
                     </a>
                     </li>
                 </div>
-                <div className="tooltip tooltip-top justify-items-center" data-tip="tiktok">
+                {/* <div className="tooltip tooltip-top justify-items-center" data-tip="tiktok">
                     <li>
                       <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.tiktok.com/@aka.jimena" target="_blank" rel="noopener noreferrer">
                         <ThinTikTokIcon />
                       </a>
                     </li>
-                </div>
+                </div> */}
                 <div className="tooltip tooltip-top justify-items-center" data-tip="portfolio">
                     <li>
                         <div 
@@ -103,16 +103,6 @@ const Footer = () => {
                         )}
                     </li>
                 </div>
-                <div className="tooltip tooltip-top justify-items-center" data-tip="youtube">
-                    <li>
-                      <div 
-                        className="hover:scale-150 hover:bg-transparent transition-transform duration-200"
-                        onClick={handleYoutubeClick}
-                      >
-                        <ThinYoutubeIcon />
-                      </div>
-                    </li>
-                </div>
                 <div className="tooltip tooltip-top justify-items-center" data-tip="email">
                     <li>
                         <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="mailto:SamanthaB.Jimenez@gmail.com" target="_blank" rel="noopener noreferrer">
@@ -141,19 +131,29 @@ const Footer = () => {
                       </a>
                     </li>
                 </div>
-                <div className="tooltip tooltip-top justify-items-center" data-tip="goodreads">
+                {/* <div className="tooltip tooltip-top justify-items-center" data-tip="goodreads">
                     <li>
                       <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://www.goodreads.com/user/show/183397302-samantha" target="_blank" rel="noopener noreferrer">
                         <GoodReadsIcon />
                       </a>
                     </li>
-                </div>
+                </div> */}
                 <div className="tooltip tooltip-top justify-items-center" data-tip="spotify">
                   <li>
                     <a className="hover:scale-150 hover:bg-transparent transition-transform duration-200" href="https://open.spotify.com/user/jimenamorenita?si=2fa3b2d8e7d147d1" target="_blank" rel="noopener noreferrer">
                         <ThinSpotifyIcon />
                     </a>
                   </li>
+                </div>
+                <div className="tooltip tooltip-top justify-items-center" data-tip="youtube">
+                    <li>
+                      <div 
+                        className="hover:scale-150 hover:bg-transparent transition-transform duration-200"
+                        onClick={handleYoutubeClick}
+                      >
+                        <ThinYoutubeIcon />
+                      </div>
+                    </li>
                 </div>
             </ul>
         </nav>
