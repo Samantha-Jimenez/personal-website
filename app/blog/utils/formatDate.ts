a717b6d05f575fa9bd22c073f2613a0aefa96112
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-04-06 10:33:26 -0400

    lint

--- app/blog/utils/formatDate.ts
+++ app/blog/utils/formatDate.ts
@@ -2,4 +2,3 @@ export const formatDate = (dateString: string) => {
     const options: Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: '2-digit' };
     return new Date(dateString).toLocaleDateString(undefined, options);
 };
-  
