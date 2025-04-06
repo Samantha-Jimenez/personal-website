483851f0f147237b70dda9204e3389f1a8124f39
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-04-06 03:38:18 -0400

    add blog utils to get posts

---
+++ app/blog/utils/getAllPosts.ts
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'app/blog/posts');
+
+export function getAllPosts() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPosts = fileNames.map((fileName) => {
+    const slug = fileName.replace(/\.md$/, '');
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    
+    const { data } = matter(fileContents);
+
+    return {
+      slug,
+      title: data.title,
+      excerpt: data.excerpt, // Ensure you have an excerpt in your Markdown files
+    };
+  });
+
+  return allPosts;
+} 
