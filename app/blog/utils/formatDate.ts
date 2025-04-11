dd74a3b53ae56a7b040d72d954899e058629f4dc
Samantha-Jimenez <samanthab.jimenez@gmail.com>
2025-04-11 17:33:25 -0400

    fix format date bug

--- app/blog/utils/formatDate.ts
+++ app/blog/utils/formatDate.ts
@@ -1,4 +1,6 @@
 export const formatDate = (dateString: string) => {
     const options: Intl.DateTimeFormatOptions = { weekday: 'long', year: 'numeric', month: 'long', day: '2-digit' };
-    return new Date(dateString).toLocaleDateString(undefined, options);
+    const date = new Date(dateString);
+    date.setHours(date.getHours() + date.getTimezoneOffset() / 60); // Adjust for timezone
+    return date.toLocaleDateString(undefined, options);
 };
