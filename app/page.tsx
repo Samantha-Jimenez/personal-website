beda3ecc59429745a7400994bd39f52e90be030a
Samantha <samanthab.jimenez@gmail.com>
2025-06-17 18:41:45 -0400

    connect widget component

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ import Toggle from 'react-toggle';
 import 'react-toggle/style.css';
 import PortfolioModal from './components/PortfolioModal';
 import MvmntModal from './components/MvmntModal';
+import InstagramEmbed from './components/InstagramEmbed';
+import YouTubeEmbed from './components/YouTubeEmbed';
+import TikTokEmbed from './components/TikTokEmbed';
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
@@ -67,24 +70,37 @@ export default function Home() {
           />
           <p className="text-sm pl-2 text-gray-200"> {darkMode ? 'Light Mode' : 'Dark Mode'} </p>
         </label>
-        <div className="relative grid grid-rows-1 grid-cols-1">
-          <div className="row-start-1 row-end-2 col-start-1 col-end-3 absolute inset-0 bg-zinc-600 opacity-70 dark:bg-zinc-800 dark:opacity-70"></div>
-          <video
-            className="row-start-1 row-end-2 col-start-1 col-end-3 h-full object-cover object-center"
-            src="https://videos.pexels.com/video-files/946146/946146-hd_1920_1080_30fps.mp4"
-            autoPlay
-            loop
-            muted
-            playsInline
-          />
-          <div className="row-start-1 col-start-1 inset-0 flex flex-col z-10 px-6 sm:px-24 py-16 h-full">
-            <p className="row-start-1 col-start-1 col-span-1 bungee-hairline-thin text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl max-w-max whitespace-nowrap max-[540px]:place-self-center min-[540px]:place-self-start text-white">Samantha</p>
-            <p className="row-start-2 col-start-1 col-span-1 bungee-hairline-thin text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl max-w-max whitespace-nowrap max-[540px]:place-self-center min-[540px]:place-self-end text-white">Jimenez</p>
-            <p className="row-start-3 col-start-1 col-span-1 montserrat-mine text-xl justify-center mt-16 md:mt-20 lg:mt-24 xl:mt-32 dark:text-gray-200 text-white">Hi, I&apos;m Sam, a full-stack software engineer with a passion for exploring, creating, and staying active. After years of focusing on my career, I&apos;m rediscovering the hobbies and interests that bring me joy—fitness, food, travel, and creative projects.
-              <br/><br/> This website is my space to document that journey, share insights, and connect with others. From blog posts and tech projects to reflections on life and adventure, I&apos;m excited to bring you along as I embrace my passions.
-              <br/><br/> Currently, I&apos;m building an app with a group of engineers from my time at Pursuit Coding Fellowship in 2019, and I can&apos;t wait to share what&apos;s next..
-            </p>
+        <div className="grid grid-cols-1">
+          <div className="relative grid">
+            <div className="row-start-1 row-end-2 col-start-1 col-end-3 absolute inset-0 opacity-70 bg-zinc-950 dark:opacity-80"></div>
+            <video
+              className="row-start-1 row-end-2 col-start-1 col-end-3 h-full object-cover object-center"
+              src="/bg-video.mp4"
+              autoPlay
+              loop
+              muted
+              playsInline
+            />
+            <div className="row-start-1 col-start-1 inset-0 flex flex-col z-10 px-24 py-16 h-full">
+              <p className="row-start-1 col-start-1 col-span-1 bungee-hairline-thin text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl whitespace-nowrap max-[540px]:place-self-center min-[540px]:place-self-start text-white overflow-visible">Samantha</p>
+              <p className="row-start-2 col-start-1 col-span-1 bungee-hairline-thin text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl whitespace-nowrap max-[540px]:place-self-center min-[540px]:place-self-end text-white">Jimenez</p>
+              <p className="row-start-3 col-start-1 col-span-1 montserrat-mine text-xl justify-center mt-32 dark:text-gray-200 text-white">Hi, I&apos;m Sam, a full-stack software engineer with a passion for exploring, creating, and staying active. After years of focusing on my career, I&apos;m rediscovering the hobbies and interests that bring me joy—fitness, food, travel, and creative projects.
+                <br/><br/> This website is my space to document that journey, share insights, and connect with others. From blog posts and tech projects to reflections on life and adventure, I&apos;m excited to bring you along as I embrace my passions.
+                <br/><br/> Currently, I&apos;m building an app with a group of engineers from my time at Pursuit Coding Fellowship in 2019, and I can&apos;t wait to share what&apos;s next..
+              </p>
+            </div>
           </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 py-8 bg-gray-100 dark:bg-zinc-900">
+          <div className="md:border-r border-gray-400 dark:border-gray-700">
+            <InstagramEmbed />
+          </div>
+          <div className="">
+            <TikTokEmbed />
+          </div>
+        </div>
+        <div className="px-24 bg-gray-100 dark:bg-zinc-900">
+          <YouTubeEmbed />
         </div>
         <div className="flex">
           <ContactForm />
