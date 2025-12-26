b69cdf2b03d8c9779402b0b136bd8522f1427194
Samantha <samanthab.jimenez@gmail.com>
2025-12-26 08:26:37 -0500

    update color, font, and connect announcement bar

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import MotevisModal from './components/MotevisModal';
 import InstagramEmbed from './components/InstagramEmbed';
 import YouTubeEmbed from './components/YouTubeEmbed';
 import TikTokEmbed from './components/TikTokEmbed';
+import AnnouncementBar from './components/AnnouncementBar';
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
@@ -57,7 +58,7 @@ export default function Home() {
   return (
     <>
       <NavBar onPortfolioClick={handlePortfolioClick} onMotevisClick={handleMotevisClick} />
-      <div className="sticky top-0 z-[11] dark:bg-emerald-600 bg-emerald-800">
+      <div className="sticky top-0 z-[11] dark:bg-emerald-700 bg-emerald-900">
         <label className="flex items-center py-2 pl-6">
           <Toggle
             icons={{
@@ -71,10 +72,10 @@ export default function Home() {
           <p className="text-base pl-2 text-gray-200"> {darkMode ? 'Light Mode' : 'Dark Mode'} </p>
         </label>
       </div>
-      <main className="flex min-h-max flex-col overflow-x-hidden bg-gray-100 dark:bg-zinc-900">
+      <main className="flex min-h-max flex-col overflow-x-hidden bg-white dark:bg-[#130E0A]">
         <div className="grid grid-cols-1">
           <div className="relative grid w-full bg-gray-100 dark:bg-zinc-900">
-            <div className="row-start-1 row-end-2 col-start-1 col-end-3 absolute inset-0 opacity-70 bg-zinc-950 dark:opacity-80"></div>
+            <div className="row-start-1 row-end-2 col-start-1 col-end-3 absolute inset-0 opacity-70 bg-[#140F0B] dark:opacity-80"></div>
             <video
               className="row-start-1 row-end-2 col-start-1 col-end-3 h-full w-full object-cover object-center"
               src="/bg-video.mp4"
@@ -84,9 +85,9 @@ export default function Home() {
               playsInline
             />
             <div className="row-start-1 col-start-1 inset-0 flex flex-col z-10 px-4 md:px-24 pt-16 pb-8 h-full w-full">
-              <p className="row-start-1 col-start-1 col-span-1 bungee-hairline-thin max-[385px]:text-5xl text-6xl md:text-7xl lg:text-8xl xl:text-9xl whitespace-nowrap max-[540px]:place-self-center min-[540px]:place-self-start text-white overflow-visible">Samantha</p>
-              <p className="row-start-2 col-start-1 col-span-1 bungee-hairline-thin max-[385px]:text-5xl text-6xl md:text-7xl lg:text-8xl xl:text-9xl whitespace-nowrap max-[540px]:place-self-center min-[540px]:place-self-end text-white">Jimenez</p>
-              <p className="row-start-3 col-start-1 col-span-1 montserrat-mine text-xl justify-center mt-16 md:mt-32 dark:text-gray-200 text-white">
+              <p className="row-start-1 col-start-1 col-span-1 chango-regular max-[415px]:text-4xl max-[540px]:text-5xl text-6xl md:text-7xl lg:text-8xl xl:text-9xl whitespace-nowrap place-self-start dark:text-[#F1CC00] text-[#CC1E00] uppercase overflow-visible text-neon-very-subtle">Samantha</p>
+              <p className="row-start-2 col-start-1 col-span-1 chango-regular max-[415px]:text-4xl max-[540px]:text-5xl text-6xl md:text-7xl lg:text-8xl xl:text-9xl whitespace-nowrap place-self-end dark:text-[#F1CC00] text-[#CC1E00] uppercase text-neon-very-subtle">Jimenez</p>
+              <p className="row-start-3 col-start-1 col-span-1 montserrat-mine text-xl justify-center mt-16 md:mt-32 text-white">
                 Hey, I&apos;m Sam, a full-stack software engineer with a creative soul and a love for movement, good food, and meaningful projects. After years of pouring into my career, I&apos;m reconnecting with the things that light me up: fitness, food, storytelling, and self-expression.
                 <br /><br />
                 This website is my space to document that journey, share insights, and connect with others. From blog posts and dev projects to life reflections and creative experiments — It&apos;s part portfolio, part digital journal.
@@ -96,19 +97,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-[1fr_auto_1fr] gap-4 py-8 px-4 md:px-24 bg-gray-100 dark:bg-zinc-900">
+        <div className="grid grid-cols-1 md:grid-cols-[1fr_auto_1fr] gap-4 py-8 px-4 md:px-24 bg-white dark:bg-[#130E0A]">
           <div className="">
             <InstagramEmbed />
           </div>
-          <div className="divider divider-horizontal hidden md:flex before:bg-gray-400 after:bg-gray-400 dark:before:bg-gray-500 dark:after:bg-gray-500"></div>
+          <div className="divider divider-horizontal hidden md:flex before:bg-stone-300 after:bg-stone-300 dark:before:bg-[#292929] dark:after:bg-[#292929]"></div>
           <div className="divider md:hidden flex before:bg-gray-400 after:bg-gray-400 dark:before:bg-gray-500 dark:after:bg-gray-500"></div>
           <div className="">
             <TikTokEmbed />
           </div>
         </div>
         <div className="divider md:hidden flex px-4 before:bg-gray-400 after:bg-gray-400 dark:before:bg-gray-500 dark:after:bg-gray-500"></div>
-        <div className="px-4 md:px-24 py-8 ">
+        <div className="px-4 md:px-24 py-8">
           <YouTubeEmbed />
+        </div>
+        <div className="flex w-full bg-[#F4F1EC] dark:bg-[#001610]">
+          <AnnouncementBar />
         </div>
         <div className="flex w-full">
           <ContactForm />
