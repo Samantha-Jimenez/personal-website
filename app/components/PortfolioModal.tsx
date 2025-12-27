737962a3980c61e06ccfdeb438da0bb4591967c6
Samantha <samanthab.jimenez@gmail.com>
2025-12-27 08:15:33 -0500

    modals dark/light mode, update font and ui

--- app/components/PortfolioModal.tsx
+++ app/components/PortfolioModal.tsx
@@ -5,14 +5,14 @@ const PortfolioModal = ({ isOpen, onClose, onConfirm }: { isOpen: boolean, onClo
 
   return (
     <dialog className="modal modal-open" onClick={onClose}>
-      <div className="modal-box" onClick={(e) => e.stopPropagation()}>
+      <div className="modal-box bg-[#F4F0E9] dark:bg-[#333333] tracking-wide roboto-mine" onClick={(e) => e.stopPropagation()}>
         <form method="dialog">
-          <button onClick={onClose} className="btn btn-circle btn-ghost absolute right-[0.1rem] top-[0.1rem] text-white">✕</button>
+          <button onClick={onClose} className="btn btn-circle btn-ghost absolute border-none right-[0.1rem] top-[0.1rem] dark:text-white text-gray-900 hover:bg-transparent hover:shadow-none hover:border-none hover:text-[#CC1E00] hover:dark:text-[#F1CC00] hover:scale-125 transition-transform duration-200">✕</button>
         </form>
-        <h2 className="font-bold text-lg text-white pb-2">Heads up,</h2>
-        <p className="text-white">You are leaving this site to go to my professional portfolio. Do you want to continue?</p>
+        <h2 className="font-bold text-lg dark:text-white pb-2 text-gray-900">Heads up,</h2>
+        <p className="dark:text-white text-gray-900">You are leaving this site to go to my professional portfolio. Do you want to continue?</p>
         <div className="modal-action">
-          <button onClick={onConfirm} className="btn">Yup!</button>
+          <button onClick={onConfirm} className="btn dark:bg-emerald-700 hover:bg-emerald-800 bg-emerald-900 border-none hover:scale-105 transition-transform duration-200">Yup!</button>
         </div>
       </div>
     </dialog>
